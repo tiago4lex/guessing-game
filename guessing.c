7560ca5a92b16137e782384b34b8b61a854e9f83
Tiago <tiago.4lexandre@gmail.com>
2025-05-06 14:27:44 -0300

    Added Ascci Art

--- guessing.c
+++ guessing.c
@@ -4,9 +4,25 @@
 
 int main()
 {
-    printf("********************************\n");
-    printf("* Welcome to my guessing game! *\n");
-    printf("********************************\n");
+    printf("\n\n");
+    printf("                        .                                   \n");
+    printf("                .                                           \n");
+    printf("                         .                                  \n");
+    printf("               /^\\     .                                   \n");
+    printf("          /\\    V                  WELCOME TO              \n");
+    printf("         /__\\   I      O  o        MY GUESSING GAME        \n");
+    printf("        //..\\  I     .                                     \n");
+    printf("        \\].`[/  I                                          \n");
+    printf("        /l\\/j\\  (]    .  O                                \n");
+    printf("       /. ~~ ,\\/I          .                               \n");
+    printf("       \\L__j^\\/I       o                                  \n");
+    printf("        \\/--v}  I     o   .                                \n");
+    printf("        |    |  I   _________                               \n");
+    printf("        |    |  I c(`       ')o                             \n");
+    printf("        |    l  I   \\.     ,/                              \n");
+    printf("      _/j  L l\\_!  _//^---^\\_                             \n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("\n\n");
 
     int guess;
     int win = 0;
@@ -18,7 +34,9 @@ int main()
 
     int difficulty;
     printf("Choose a difficulty level:\n");
-    printf("(1) - Easy (2) - Medium (3) - Hard\n\n");
+    printf("1 - Easy (20 attempts)\n");
+    printf("2 - Medium (15 attempts)\n");
+    printf("3 - Hard (6 attempts)\n");
     printf("Enter the difficulty level: ");
     scanf("%d", &difficulty);
 
@@ -80,13 +98,37 @@ int main()
 
     if (win)
     {
+        printf("\n\n");
+        printf("             OOOOOOOOOOO               \n");
+        printf("         OOOOOOOOOOOOOOOOOOO           \n");
+        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+        printf("    OOOOOO      OOOOO      OOOOOO      \n");
+        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+        printf("         OOOOOO         OOOOOO         \n");
+        printf("             OOOOOOOOOOOO              \n");
+        printf("\n\n");
+
         printf("Nice, you guessed the right number!\n");
         printf("You guessed the number in %d attempts!\n", attempts);
-        printf("Your total score is %.1f points!\n", points);
-        printf("Play again, you won't be that lucky the next time!\n");
+        printf("Your total score is %.2f points!\n", points);
+        printf("Play again, you won't be that lucky the next time!\n\n");
     }
     else
     {
+        printf("\n\n");
+        printf("       \\|/ ____ \\|/    \n");
+        printf("        @~/ ,. \\~@      \n");
+        printf("       /_( \\__/ )_\\    \n");
+        printf("          \\__U_/        \n");
+        printf("\n\n");
         printf("Sorry, you lost!\n");
         printf("The secret number was %d\n", secret_number);
         printf("Better luck next time!\n");
