fdf67afe444502efc106d738f563ccdbebcd7385
Tiago <tiago.4lexandre@gmail.com>
2025-05-06 14:07:10 -0300

    Added difficulty levels

--- guessing.c
+++ guessing.c
@@ -16,8 +16,32 @@ int main()
     srand(time(0));                   // Seed the random number generator with the current time
     int secret_number = rand() % 100; // Generate a random number between 0 and 99
 
-    while (!win)
+    int difficulty;
+    printf("Choose a difficulty level:\n");
+    printf("(1) - Easy (2) - Medium (3) - Hard\n\n");
+    printf("Enter the difficulty level: ");
+    scanf("%d", &difficulty);
+
+    int max_attempts;
+    switch (difficulty)
     {
+    case 1:
+        max_attempts = 20;
+        break;
+    case 2:
+        max_attempts = 15;
+        break;
+    case 3:
+        max_attempts = 6;
+        break;
+    default:
+        printf("Invalid difficulty level!\n");
+        return 1; // Exit the program with an error code
+    }
+
+    for (int i = 1; i <= max_attempts; i++)
+    {
+        printf("Attempt %d of %d\n", i, max_attempts);
 
         printf("Enter your guess no. %d (0-99): ", attempts);
         scanf("%d", &guess);
@@ -35,8 +59,6 @@ int main()
 
         if (win)
         {
-            printf("Nice, you guessed the right number!\n");
-            printf("Play again, you won't be that lucky the next time!\n");
             break;
         }
         else if (higher)
@@ -55,6 +77,18 @@ int main()
     }
 
     printf("End of game!\n");
-    printf("You guessed the number in %d attempts!\n", attempts);
-    printf("Your total score is %.1f points!\n", points);
+
+    if (win)
+    {
+        printf("Nice, you guessed the right number!\n");
+        printf("You guessed the number in %d attempts!\n", attempts);
+        printf("Your total score is %.1f points!\n", points);
+        printf("Play again, you won't be that lucky the next time!\n");
+    }
+    else
+    {
+        printf("Sorry, you lost!\n");
+        printf("The secret number was %d\n", secret_number);
+        printf("Better luck next time!\n");
+    }
 }
